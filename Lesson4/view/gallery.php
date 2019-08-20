48c6af22dfbbd1e4f63bf586511e8435535a7455
GrigoriyGolikov <gregor@inbox.ru>
2019-08-20 15:58:57 +0300

    правки

--- Lesson4/view/gallery.php
+++ Lesson4/view/gallery.php
@@ -3,7 +3,7 @@
     <div class="post_title"><h2>Моя галерея</h2></div>
     <div class="gallery">
         <?php foreach ($images as $item): ?>
-        <a rel="gallery" class="photo" href=<?=IMG_BIG_DIR.$item?>><img src="<?=IMG_SML_DIR.$item?>" width="150" height="100" /></a>
+        <a rel="gallery" class="photo" href=<?=IMG_BIG_DIR . $item?>><img src="<?=IMG_SML_DIR . $item?>" width="150" height="100" /></a>
         <?php endforeach;?>
     </div>
 </div>
