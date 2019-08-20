86cfdf6aabf9ee9a3eebae531fca1e8adb3d55b2
GrigoriyGolikov <gregor@inbox.ru>
2019-08-20 10:08:35 +0300

    lesson4

--- Lesson4/config/config.php
+++ Lesson4/config/config.php
@@ -1,5 +1,8 @@
 <?php
-define("TEMPLATES_DIR", "../view/");
+define("ROOT_DIR", $_SERVER['DOCUMENT_ROOT']);
+define("TEMPLATES_DIR", ROOT_DIR."/../view/");
+define("IMG_BIG_DIR", "/gallery_img/big/");
+define("IMG_SML_DIR", "/gallery_img/small/");
 
-require_once "../engine/functions.php";
-require_once "../engine/log.php";
+require_once ROOT_DIR."/../engine/functions.php";
+require_once ROOT_DIR."/../engine/log.php";
